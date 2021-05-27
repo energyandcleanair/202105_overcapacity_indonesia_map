f39a43152f5368b0136a3d62030708733b9bfef8
Hubert Thieriot <hubert@energyandcleanair.org>
2021-05-27 13:35:45 +0800

    -works with latest results

--- data/xlsx_to_geojson.R
+++ data/xlsx_to_geojson.R
@@ -2,14 +2,15 @@ library(jsonlite)
 library(dplyr)
 library(sf)
 setwd("~/development/crea/studies/202105_overcapacity_indonesia_map/data/")
-r <- readxl::read_xlsx("05202021_overcapacity_results.xlsx")
-r$`Overcapacity costs (MM USD)` <- r$`Overcapacity costs (USD)`/1e6
+r <- readxl::read_xlsx("latest_results.xlsx")
+r$`FOM (MM USD)` <- r$`FOM ($)`/1e6
+r$`Overcapacity (MW)` <- r$`Capacity (MW)`
 
-plant_id_cols <- c("Country", "Subnational unit (province, state)", "Plant", "Sponsor", "Parent", "Location")
+
+plant_id_cols <- c("Country", "Subnational unit (province, state)", "Plant", "Sponsor", "Parent", "Location", "ParentID")
 r <- r %>%
 	group_by_at(plant_id_cols) %>%
-	summarise_at(c("Capacity (MW)", "Overcapacity (MW)", "Refurbishment cost ($/kW)",
-		"Overcapacity costs (USD)", "Overcapacity costs (MM USD)"), sum) %>%
+	summarise_at(c("Overcapacity (MW)", "FOM (MM USD)"), sum) %>%
 	left_join(r %>% group_by_at(plant_id_cols) %>% summarise_at(c("Longitude","Latitude"), mean))
 
 
